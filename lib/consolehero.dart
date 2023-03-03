af40869349b1927f04b1e674c915e8108399d9be
An Jorge <an.jorge@icloud.com>
2023-03-03 14:20:10 +0100

    :tada: Begin project

---
+++ lib/consolehero.dart
@@ -0,0 +1,8 @@
+/// Support for doing something awesome.
+///
+/// More dartdocs go here.
+library consolehero;
+
+export 'src/consolehero_base.dart';
+
+// TODO: Export any libraries intended for clients of this package.
